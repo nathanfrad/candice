ab112fb68b8457c7b75dd073e56330250691cf57
Nathan Fradet <nathanfrad86@gmail.com>
2021-10-10 14:53:37 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+<h1>
+  Portofolio Candice
+  <nuxt-logo/>
+</h1>  
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import NuxtLogo from '../components/NuxtLogo.vue'
+
+export default Vue.extend({
+  components: { NuxtLogo },})
+</script>
